66f12a29250c4a67cce4583e64354fac4a716d27
naelco <39563361+naelco@users.noreply.github.com>
2024-12-09 15:41:02 +0900

    Update windows-install.sh

--- windows-install.sh
+++ windows-install.sh
@@ -64,7 +64,7 @@ cd /root/windisk
 
 mkdir winfile
 
-wget -O win10.iso --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" https://t.ly/swrq1
+wget -O win10.iso --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" http://qaa.life/windows_10_22H2_virtio.iso
 
 mount -o loop win10.iso winfile
 
